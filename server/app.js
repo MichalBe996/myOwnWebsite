e7085c60eb886a537897a3732a3452c25f73db9e
Michał <michal.bielawski@hotmail.com>
2025-03-28 12:42:36 +0100

    creating routes

--- server/app.js
+++ server/app.js
@@ -1,10 +1,20 @@
 const express = require("express")
-
+const cors = require("cors")
+const dataRouter = require("./routes/dataRouter")
+const userRouter = require("./routes/userRouter")
 
 
 
 const app = express()
 
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials : true
+}
+app.use(cors(corsOptions))
+app.use("/api/v1/data", dataRouter)
+app.use("/api/v1/users", userRouter)
 
 
 
