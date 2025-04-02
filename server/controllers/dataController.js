12859977101c9232070a1fad31b5921aaae0e191
Michał <michal.bielawski@hotmail.com>
2025-04-02 14:05:06 +0200

    created data entry model

---
+++ server/controllers/dataController.js
@@ -0,0 +1,34 @@
+
+
+
+
+const getAllData = async (req, res, next) => {
+
+
+    try {
+        
+    } catch (error) {
+        
+    }
+
+}
+
+const getSinglelData = async (req, res, next) => {
+
+
+    try {
+        
+    } catch (error) {
+        
+    }
+
+}
+
+
+const deleteSingleData = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
